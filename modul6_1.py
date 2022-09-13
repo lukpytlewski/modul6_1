a14d8fefeb02d1ac7142e9c44142c3d62829e512
Lukasz Pytlewski <lukpytlewski@yahoo.com>
2022-09-13 11:36:12 +0200

    Dodanie funkcji aktualizających bazę danych oraz kasujących

--- modul6_1.py
+++ modul6_1.py
@@ -108,6 +108,67 @@ def select_where(conn, table, **query):
     return rows
 
 
+def update(conn, table, id, **kwargs):
+    """
+    update sukces and data_wyprawy of a wyprawy
+    :param conn:
+    :param table: table name
+    :param id: row id
+    :return:
+    """
+    parameters = [f"{k} = ?" for k in kwargs]
+    parameters = ", ".join(parameters)
+    values = tuple(v for v in kwargs.values())
+    values += (id,)
+
+    sql = f""" UPDATE {table}
+             SET {parameters}
+             WHERE id = ?"""
+    try:
+        cur = conn.cursor()
+        cur.execute(sql, values)
+        conn.commit()
+        print("OK")
+    except sqlite3.OperationalError as e:
+        print(e)
+
+
+def delete_where(conn, table, **kwargs):
+    """
+    Delete from table where attributes from
+    :param conn:  Connection to the SQLite database
+    :param table: table name
+    :param kwargs: dict of attributes and values
+    :return:
+    """
+    qs = []
+    values = tuple()
+    for k, v in kwargs.items():
+        qs.append(f"{k}=?")
+        values += (v,)
+    q = " AND ".join(qs)
+
+    sql = f"DELETE FROM {table} WHERE {q}"
+    cur = conn.cursor()
+    cur.execute(sql, values)
+    conn.commit()
+    print("Deleted")
+
+
+def delete_all(conn, table):
+    """
+    Delete all rows from table
+    :param conn: Connection to the SQLite database
+    :param table: table name
+    :return:
+    """
+    sql = f"DELETE FROM {table}"
+    cur = conn.cursor()
+    cur.execute(sql)
+    conn.commit()
+    print("Deleted")
+
+
 if __name__ == "__main__":
 
     create_szczyty_sql = """
@@ -162,5 +223,12 @@ if __name__ == "__main__":
 
     # wszystkie wyprawy zakończone sukcesem
     select_where(conn, "wyprawy", sukces=True)
+
+    update(conn, "wyprawy", 2, sukces="false")
+
+    delete_where(conn, "wyprawy", id=3)
+
+    # poniższej funkcji mogę użyć, ale nie chcę :-)
+    # delete_all("wyprawy")
 
     conn.close()
